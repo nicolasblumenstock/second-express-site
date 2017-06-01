d37bb136f7d85f48c1e69e95dba911adf29bc130
Nicolas Blumenstock <nicpblum@gmail.com>
2017-06-01 15:46:15 -0400

    added and modified old weather app to function with the backend

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,93 +1,48 @@
 $(document).ready(function(){
-	// var canvas = $('#weather-canvas');
-	// var context = canvas[0].getContext('2d');
-	var mainUrl = `http://api.openweathermap.org/data/2.5/weather?zip=`
-	var forecastUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?zip=`
 	var dates = ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat'];
-	// function animateCircle(maths,currentTemp){
-	// 	context.fillStyle = '#582b11';
-	// 	context.beginPath();
-	// 	context.arc(250,175,70,Math.PI*0, Math.PI*2);
-	// 	context.fill();
-	// 	context.closePath();
-
-	// 	context.lineWidth = 15;
-	// 	context.strokeStyle = '#AF125A';
-	// 	context.beginPath();
-	// 	context.arc(250,175,75,Math.PI*1.5,(Math.PI*2*(maths/100))+ Math.PI*1.5);
-	// 	context.stroke();
-	// 	context.closePath();		
-	// 	maths++;
-	// 	if (maths < currentTemp){
-	// 	requestAnimationFrame(function(){
-	// 		animateCircle(maths,currentTemp);
-	// 	});
-	// 	}
-	// }
-
-	// animateCircle(0,0);
+	var currTemps = {
+		curr: Math.round(currentDay.main.temp),
+		high: currentDay.main.temp_max,
+		low: currentDay.main.temp_min,
+	}
+	var locationName = currentDay.name;
+	var currWeather = currentDay.weather[0].description;
+	var currIcon = currentDay.weather[0].icon + '.png';
+	var currDate = new Date((currentDay.dt)*1000);
+	var currHumidity = currentDay.main.humidity;
+	var currWindSpeed = currentDay.wind.speed;
+	var currWindDirection = currentDay.wind.deg;
+	var currentRise = new Date((currentDay.sys.sunrise)*1000);
+	var currentRiseTime = currentRise.toLocaleTimeString();
+	var currentSet = new Date((currentDay.sys.sunset)*1000);
+	var currentSetTime = currentSet.toLocaleTimeString();
 
 
-	$('.weather-submit').submit(function(event){
-		// context.clearRect(0,0,500,500);
-		event.preventDefault();
-		var location = $('#weather-location').val()
-		var url = `${mainUrl}${location},us&units=imperial&appid=${apiKey}`
-		var multidayUrl = `${forecastUrl}${location},us&units=imperial&cnt=5&appid=${apiKey}`
-		$.getJSON(url,(data)=>{
-			var currTemps = {
-				curr: Math.round(data.main.temp),
-				high: data.main.temp_max,
-				low: data.main.temp_min,
-			}
-			var locationName = data.name;
-			var currWeather = data.weather[0].description;
-			var currIcon = data.weather[0].icon + '.png';
-			var currDate = new Date((data.dt)*1000);
-			var currHumidity = data.main.humidity;
-			var currWindSpeed = data.wind.speed;
-			var currWindDirection = data.wind.deg;
-			var currentRise = new Date((data.sys.sunrise)*1000);
-			var currentRiseTime = currentRise.toLocaleTimeString();
-			var currentSet = new Date((data.sys.sunset)*1000);
-			var currentSetTime = currentSet.toLocaleTimeString();
+	$('#tabday1').html(dates[currDate.getDay()])
+	$('.weatherIcon').html(`<img src="http://openweathermap.org/img/w/${currIcon}" />`);
+	$('.currentTemp').html(currTemps.curr + '&#8457;')
+	$('.weather-description').html(currWeather)
+	$('.location').html(locationName);
+	$('.humidity').html(currHumidity);
+	$('.wind').html(currWindSpeed);
+	$('.direction').html(currWindDirection);
+	$('.current-min').html(currTemps.low + '&#8457;');
+	$('.current-max').html(currTemps.high + '&#8457;');
+	$('.current-sunrise').html(currentRiseTime);
+	$('.current-sunset').html(currentSetTime);			
+
+	for (let i = 1; i < multiDay.list.length; i++){
+		var dailyMin = multiDay.list[i].temp.min;
+		var dailyMax = multiDay.list[i].temp.max;
+		var dailyWeather = multiDay.list[i].weather[0].main;
+		var dailyIcon = multiDay.list[i].weather[0].icon + '.png';
+		var dailyDate = new Date((multiDay.list[i].dt) * 1000);
 
 
+		$('#day'+(i+1)).html(`min: ${dailyMin}<br>max: ${dailyMax}<br>weather: ${dailyWeather}<br><img src='http://openweathermap.org/img/w/${dailyIcon}' />`)
+		$('#tabday'+(i+1)).html(dates[dailyDate.getDay()]);
+	}
 
-
-			// animateCircle(0,currTemps.curr);
-
-			$('#tabday1').html(dates[currDate.getDay()])
-			$('.weatherIcon').html(`<img src="http://openweathermap.org/img/w/${currIcon}" />`);
-			$('.currentTemp').html(currTemps.curr + '&#8457;')
-			$('.weather-description').html(currWeather)
-			$('.location').html(locationName);
-			$('.humidity').html(currHumidity);
-			$('.wind').html(currWindSpeed);
-			$('.direction').html(currWindDirection);
-			$('.current-min').html(currTemps.low + '&#8457;');
-			$('.current-max').html(currTemps.high + '&#8457;');
-			$('.current-sunrise').html(currentRiseTime);
-			$('.current-sunset').html(currentSetTime);			
-
-
-
-		})
-
-		$.getJSON(multidayUrl,(otherData)=>{
-			for (let i = 1; i < otherData.list.length; i++){
-				var dailyMin = otherData.list[i].temp.min;
-				var dailyMax = otherData.list[i].temp.max;
-				var dailyWeather = otherData.list[i].weather[0].main;
-				var dailyIcon = otherData.list[i].weather[0].icon + '.png';
-				var dailyDate = new Date((otherData.list[i].dt) * 1000);
-
-				$('#day'+(i+1)).html(`min: ${dailyMin}<br>max: ${dailyMax}<br>weather: ${dailyWeather}<br><img src='http://openweathermap.org/img/w/${dailyIcon}' />`)
-				$('#tabday'+(i+1)).html(dates[dailyDate.getDay()]);
-			}
-		})
-
-	})
 
 	$('#myTabs a[href="#day1"]').click(function(event){
 		event.preventDefault();
--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,12 @@ var students = [
 	'Hayes',
 	'Michael'
 	];
+var mainUrl = `http://api.openweathermap.org/data/2.5/weather?zip=`
+var forecastUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?zip=`
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -32,8 +38,25 @@ router.get('/reverse', function(req, res, next) {
 
 router.get('/weather', function(req, res, next) {
 	res.render('weather', {
-		config: config.apiKey
+		// config: config.apiKey
 	});
+});
+
+router.post('/weather', (req,res)=>{
+	var zipCode = req.body.zipCode;
+	var currentDayUrl = `${mainUrl}${zipCode},us&units=imperial&appid=${config.apiKey}`;
+	var multiDayUrl = `${forecastUrl}${zipCode},us&units=imperial&cnt=5&appid=${config.apiKey}`;
+	request.get(currentDayUrl,(error,response,currentData)=>{
+		var currentDay = JSON.parse(currentData)
+		request.get(multiDayUrl,(error,response,multiData)=>{
+			var multiDay = JSON.parse(multiData)
+			res.render('weather_loaded',{
+				currentDay: currentDay,
+				multiDay: multiDay,
+			})
+		})
+	})
+
 })
 
 module.exports = router;
